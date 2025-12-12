2eadd4c4ef99ed127f427dfcc44c70970f97da0f
kp12032002 <keerthiprasadbe2002@gmail.com>
2025-12-12 14:22:22 +0530

    updated js

--- collections.js
+++ collections.js
@@ -11,3 +11,25 @@ menuIcon.addEventListener("click", function(){
 closeNav.addEventListener("click", function(){
     sideNav.style.right = "-50%"
 })
+
+// Product Search Functionality
+
+let productContainer = document.getElementById("productContainer")
+let search = document.getElementById("search")
+let productList = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function(){
+    let enteredValue = event.target.value.toUpperCase()
+
+    for(count = 0; count<productList.length; count = count+1)
+    {
+        var productName = productList[count].querySelector("h5").textContent
+
+        if(productName.toUpperCase().indexOf(enteredValue)<0)
+        {
+            productList[count].style.display="none"
+        } else {
+            productList[count].style.display="block"
+        }
+    }
+})
